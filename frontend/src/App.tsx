c20f9c4e56f789502020d31894509f0bc4bfd229
bluef1ash <liangtian_2005@163.com>
2026-07-21 17:28:34 +0800

    fix(前端): 修正 App 视图 import 路径以匹配目录重组
    视图已从扁平文件(XxxView.tsx)重组为目录(Xxx/index.tsx),
    App.tsx 的 import 未同步,导致 tsc 找不到模块

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,32 @@
-import React from "react";
-import { IconSprite } from "./icons/Icons";
-import { TitleBar } from "./components/TitleBar";
-import { StatusBar } from "./components/StatusBar";
-import { Stepper } from "./components/Stepper";
-import { useAppStore } from "./store/appStore";
-import { HomeView } from "./views/HomeView";
-import { ScanningView } from "./views/ScanningView";
-import { ResultView } from "./views/ResultView";
-import { ProcessingView } from "./views/ProcessingView";
-import { CompletedView } from "./views/CompletedView";
+import type React from 'react'
+import { StatusBar } from './components/StatusBar'
+import { Stepper } from './components/Stepper'
+import { TitleBar } from './components/TitleBar'
+import { IconSprite } from './icons/Icons'
+import { useAppStore } from './store/appStore'
+import { CompletedView } from './views/Completed'
+import { HomeView } from './views/Home'
+import { ProcessingView } from './views/Processing'
+import { ResultView } from './views/Result'
+import { ScanningView } from './views/Scanning'
 
 const App: React.FC = () => {
-  const view = useAppStore((s) => s.view);
+  const view = useAppStore((s) => s.view)
   return (
-    <div className='h-full flex flex-col'>
+    <div className="h-full flex flex-col">
       <IconSprite />
       <TitleBar />
-      {view !== "home" && <Stepper />}
-      <main className='flex-1 overflow-auto p-6'>
-        {view === "home" && <HomeView />}
-        {view === "scanning" && <ScanningView />}
-        {view === "result" && <ResultView />}
-        {view === "processing" && <ProcessingView />}
-        {view === "completed" && <CompletedView />}
+      {view !== 'home' && <Stepper />}
+      <main className="flex-1 overflow-auto p-6">
+        {view === 'home' && <HomeView />}
+        {view === 'scanning' && <ScanningView />}
+        {view === 'result' && <ResultView />}
+        {view === 'processing' && <ProcessingView />}
+        {view === 'completed' && <CompletedView />}
       </main>
       <StatusBar />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
